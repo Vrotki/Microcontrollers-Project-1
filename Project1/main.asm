147461778e5b9c0d60d3a11d26363d8d15747821
Vrotki <51521287+Vrotki@users.noreply.github.com>
2023-10-13 17:59:35 -0500

    Added button release logic

--- Project1/main.asm
+++ Project1/main.asm
@@ -22,7 +22,7 @@ start:
 	SBI PORTC, 0; pull-up PA0
 
 	CBI DDRA, 1; set PA1 to input
-	SBI PORTC, 1; pull-up PA1
+	SBI PORTA, 1; pull-up PA1
 
 	;configure PE4 to send output to speaker
 	SBI DDRE, 4; set PE4 to output
@@ -33,24 +33,46 @@ start:
 	OUT DDRD, R17; set all PD bits to output
 	OUT PORTD, R16; set PORTD to initial count
 
+	LDI R18, 0b11111111; set previous buttons to not pressed
+	LDI R19, 0b11111111; set current buttons to not pressed
+
 	MAIN_LOOP: CALL INC_COUNT
-		RJMP MAIN_LOOP
+		MOV R20, R18; copies previous buttons pressed values to R20 for comparison
+		IN R19, PINA; set R19 to current buttons pressed
+		MOV R21, R19; copies current buttons pressed values to R21 for comparison
 		
-		
-	;main loop:
 		;monitor PINC, 0 - if bit was 0 and is now 1, positive key was just released and the counter should be incremented, returning to 0 if passing 30
+		LSR R21; shifts positive key current pressed to carry bit
+		BRSH POSITIVE_NOT_RELEASED; branch if c == 0 - skip if button is still being pressed
+		LSR R22; shifts positive key previous pressed to carry bit
+		BRLO POSITIVE_NOT_RELEASED; branch if c == 1 - skip if button was not being pressed
+		
+		CALL INC_COUNT; increment if button was being pressed and is no longer being pressed
+		POSITIVE_NOT_RELEASED:
+
 		;monitor PINC, 1 - if bit was 0 and is now 1, negative key was just released and the counter should be decremented, returning to 30 if passing 0
+		LSR R21; shifts negative key current pressed to carry bit
+		BRSH NEGATIVE_NOT_RELEASED; branch if c == 0 - skip if button is still being pressed
+		LSR R22; shifts negative key previous pressed to carry bit
+		BRLO NEGATIVE_NOT_RELEASED; branch if c == 1 - skip if button was not being pressed
+		
+		CALL DEC_COUNT; decrement if button was being pressed and is no longer being pressed
+		NEGATIVE_NOT_RELEASED:
+
+		MOV R18, R19; moves current buttons pressed to previous buttons pressed
 
 		;if counter moved past 0 or 30, send 1000 HZ square wave to PE4 for some amount of time
 
 		;display LED's for 000xxxxx counter value by setting PORTD to the counter value
 
-; For your first project, you will design a simple, self-contained AVR-based device (Simon Board) that will, at a minimum, monitor two keys – one is a positive key that
-; increments the counter, and the other is a negative key that decrements the counter; display the current count in binary on a set of 5 LEDs; and sound an alarm when
-; the count “turns over” (cycles from a binary 30 to 0 or 0 to 30). The counter should be 0 initially.
+		RJMP MAIN_LOOP
 
 .ORG 400
 INC_COUNT: RET
 
 .ORG 500
 DEC_COUNT: RET
+
+; For your first project, you will design a simple, self-contained AVR-based device (Simon Board) that will, at a minimum, monitor two keys – one is a positive key that
+; increments the counter, and the other is a negative key that decrements the counter; display the current count in binary on a set of 5 LEDs; and sound an alarm when
+; the count “turns over” (cycles from a binary 30 to 0 or 0 to 30). The counter should be 0 initially.
