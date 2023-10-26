5924bd3ef86f2e2a8964735d3164d55753454860
Vrotki <51521287+Vrotki@users.noreply.github.com>
2023-10-26 17:02:59 -0500

    Fixed timer delay, formatting

--- Project1/main.asm
+++ Project1/main.asm
@@ -11,29 +11,29 @@ start:
 	;LED's can be in 3x3 or 2x4 pattern, based on a switch on the board
 	; - if using 2x4, PD0 goes to LED1, PD1 goes to LED2, PD2 goes to LED3, PD3 goes to LED4B, and PD4 goes to LED6B
 
-	LDI R16, 0; counter
-	;configure PA0 and PA1 to take input from buttons, with pull-up enabled
 	LDI R17, 0x00
 	OUT DDRA, R17; set PA to input
 	LDI R17, 0xFF
 	OUT PORTA, R17; pull-up PA
 
-	;configure PE4 to send output to speaker
+	; Configure PE4 to send output to speaker
 	SBI DDRE, 4; set PE4 to output
 	CBI PORTE, 4; clear PE4
 
-	;configure all PD bits to send output to LED's
+	; Configure all PD bits to send output to LED's
 	LDI R17, 0b11111111
 	OUT DDRD, R17; set all PD bits to output
 	OUT PORTD, R16; set PORTD to initial count
 
-	LDI R18, 0b11111111; set previous buttons to not pressed
-	LDI R19, 0b11111111; set current buttons to not pressed
+	LDI R18, 0b11111111; Set previous buttons to not pressed
+	LDI R19, 0b11111111; Set current buttons to not pressed
+
+	LDI R16, 0; initialize counter
 
 	; Configure registers for individual features
 
-	;Viktor Butkovich individual feature - when the 3rd button is pressed, toggle a timer than, when activated, increments the counter by 1 every second
-	;Feature requires timer 0, button SW3, and the R2, R3, and R4 registers, along with temporarily using the R25 register but not needing to keep any values stored there
+	; Viktor Butkovich individual feature - when the 3rd button is pressed, toggle a timer than, when activated, increments the counter by 1 every second
+	; Feature requires timer 0, button SW3, and the R2, R3, and R4 registers, along with temporarily using the R25 register but not needing to keep any values stored there
 	LDI R25, -125; calculated that a delay of 0.02 seconds would require a 156.25 clock cycle timer - based on testing, 125 clock cycles achieves timing closer to the desired result
 	MOV R2, R2;
 	LDI R25, 0
@@ -42,27 +42,27 @@ start:
 	
 
 	MAIN_LOOP:
-		MOV R20, R18; copies previous buttons pressed values to R20 for comparison
-		IN R19, PINA; set R19 to current buttons pressed
-		MOV R21, R19; copies current buttons pressed values to R21 for comparison
+		MOV R20, R18; Copies previous buttons pressed values to R20 for comparison
+		IN R19, PINA; Sets R19 to current buttons pressed
+		MOV R21, R19; Copies current buttons pressed values to R21 for comparison
 
-		;monitor PINA, 0 - if bit was 0 and is now 1, positive key was just released and the counter should be incremented, returning to 0 if passing 30
-		LSR R20; shifts positive key previous pressed to carry bit
-		BRLO FINISH_POSITIVE_SHIFT; branch if c == 1 - skip if button was not being pressed
-								  ; while INC_COUNT needs to be skipped, subsequent buttons will only work if R21 is still
-		LSR R21; shifts positive key current pressed to carry bit
-		BRSH POSITIVE_NOT_RELEASED; branch if c == 0 - skip if button is still being pressed
-		CALL INC_COUNT; increment if button was being pressed and is no longer being pressed
+		; Monitors PINA, 0 - if bit was 0 and is now 1, positive key was just released and the counter should be incremented, returning to 0 if passing 30
+		LSR R20; Shifts positive key previous pressed to carry bit
+		BRLO FINISH_POSITIVE_SHIFT; Branches if c == 1 - skips if button was not being pressed
+								  ; While INC_COUNT needs to be skipped, subsequent buttons will only work if R21 is still
+		LSR R21; Shifts positive key current pressed to carry bit
+		BRSH POSITIVE_NOT_RELEASED; Branches if c == 0 - skip if button is still being pressed
+		CALL INC_COUNT; Increments if button was being pressed and is no longer being pressed
 		POSITIVE_NOT_RELEASED:
 
-		;monitor PINA, 1 - if bit was 0 and is now 1, negative key was just released and the counter should be decremented, returning to 30 if passing 0
-		LSR R20; shifts negative key previous pressed to carry bit
-		BRLO FINISH_NEGATIVE_SHIFT; branch if c == 1 - skip if button was not being pressed
-								  ; while DEC_COUNT needs to be skipped, subsequent buttons will only work if R21 is still shifted
-		LSR R21; shifts negative key current pressed to carry bit
-		BRSH NEGATIVE_NOT_RELEASED; branch if c == 0 - skip if button is still being pressed
+		; Monitors PINA, 1 - if bit was 0 and is now 1, negative key was just released and the counter should be decremented, returning to 30 if passing 0
+		LSR R20; Shifts negative key previous pressed to carry bit
+		BRLO FINISH_NEGATIVE_SHIFT; Branches if c == 1 - skip if button was not being pressed
+								  ; While DEC_COUNT needs to be skipped, subsequent buttons will only work if R21 is still shifted
+		LSR R21; Shifts negative key current pressed to carry bit
+		BRSH NEGATIVE_NOT_RELEASED; Branches if c == 0 - skip if button is still being pressed
 		
-		CALL DEC_COUNT; decrement if button was being pressed and is no longer being pressed
+		CALL DEC_COUNT; Decrements if button was being pressed and is no longer being pressed
 		NEGATIVE_NOT_RELEASED:
 
 		; Detect button releases for individual features
@@ -70,9 +70,9 @@ start:
 		BRLO FINISH_TIMER_SHIFT; 
 		LSR R21
 		BRSH TIMER_NOT_RELEASED
-		COM R3; toggle timer enable
-		SBRC R3, 0; if timer not enabled (if R3 bit 0 is 0), skip
-		RCALL VIKTOR_ENABLE_TIMER; if timer just enabled, start timer loop
+		COM R3; Toggles timer enable
+		SBRC R3, 0; If timer not enabled (if R3 bit 0 is 0), skips
+		RCALL VIKTOR_ENABLE_TIMER; If timer just enabled, starts timer loop
 		TIMER_NOT_RELEASED:
 
 		; Format to add new feature called featurename on the next available button - put the code here
@@ -84,14 +84,12 @@ start:
 		featurename_NOT_RELEASED:
 
 		; Update any timers
-		SBRC R3, 0; if timer not enabled (if R3 bit 0 is 0), skip
-		RCALL VIKTOR_UPDATE_TIMER; if timer enabled, update timer
+		SBRC R3, 0; If timer not enabled (if R3 bit 0 is 0), skip
+		RCALL VIKTOR_UPDATE_TIMER; If timer enabled, update timer
 
-		MOV R18, R19; moves current buttons pressed to previous buttons pressed
+		MOV R18, R19; Moves current buttons pressed to previous buttons pressed
 
-		;if counter moved past 0 or 30, send 1000 HZ square wave to PE4 for some amount of time
-
-		;display LED's for 000xxxxx counter value by setting PORTD to complement of the counter value
+		; Display LED's for 000xxxxx counter value by setting PORTD to complement of the counter value
 		COM R16
 		OUT PORTD, R16
 		COM R16
@@ -115,11 +113,11 @@ INC_COUNT:
 	INC R16
 	LDI R25, 32
 
-	SBRC R16, 5; if 32's place is 1, overflow just occurred
-	RCALL SOUND; if overflowed, play sound and return to 0
+	SBRC R16, 5; If 32's place is 1, overflow just occurred
+	RCALL SOUND; If overflowed, play sound
 
 	SBRC R16, 5
-	LDI R16, 0
+	LDI R16, 0; If overflowed, return counter to 0
 
 	CALL BUTTON_DELAY
 	RET
@@ -128,20 +126,21 @@ INC_COUNT:
 DEC_COUNT:
 	DEC R16
 
-	SBRC R16, 7; if 128's place is 1, overflow just occurred
-	RCALL SOUND; if overflowed, play sound and return to 31
+	SBRC R16, 7; If 128's place is 1, overflow just occurred
+	RCALL SOUND; If overflowed, play sound
 
-	SBRC R16, 5
-	LDI R16, 31
+	SBRC R16, 5; If overflowed, return counter to 30
+	LDI R16, 30
 
 	CALL BUTTON_DELAY
 	RET
 
 .ORG 450
-SOUND_DELAY: ;for 1000 HZ frequency, each half-wave should have a 0.5 ms delay, or about 8000 machine cycles: 32 * 250 = 8000
-	LDI R29, 32 ;incorrect calculation - remember that each inner loop is more than 1 machine cycle
+SOUND_DELAY: ; For 1000 HZ frequency, each half-wave should have a 0.5 ms delay, or about 8000 machine cycles
+			 ; 65 and 40 were calculated to result in 8002 machine cycles for each RCALL SOUND_DELAY
+	LDI R29, 65
 	repeat_1:
-	LDI R30, 32
+	LDI R30, 40
 	repeat_2:
 	DEC R30
 	BRNE repeat_2
@@ -180,19 +179,19 @@ BUTTON_DELAY:  LDI R31, 10
 		RET
 
 .ORG 650
-VIKTOR_UPDATE_TIMER:; checks each main loop for whether a 0.02 second timer
+VIKTOR_UPDATE_TIMER:; Checks each main loop for whether a 0.02 second timer
 	IN R25, TIFR0
-	SBRC R25, TOV0; skip command if TOV0 is 0
-	RCALL VIKTOR_COMPLETE_TIMER; if TOV0, 0.02 second timer just completed
+	SBRC R25, TOV0; Skips command if TOV0 is 0
+	RCALL VIKTOR_COMPLETE_TIMER; If TOV0 is 1, 0.02 second timer just completed
 	RET
 
 .ORG 675
-VIKTOR_COMPLETE_TIMER:; resolves a 0.02 second timer ending
-	DEC R4; decrement timer loop each time timer finishes
-	BRNE CONTINUE_TIMER_LOOP
-	; if 1 second timer loop not done, start timer again
+VIKTOR_COMPLETE_TIMER:; Resolves a 0.02 second timer ending
+	DEC R4; Decrements timer loop each time timer finishes
+	BRNE CONTINUE_TIMER_LOOP; If 1 second timer loop not done, start timer again
+	
 
-	RCALL VIKTOR_START_TIMER_LOOP; if 1 second timer loop reached 0, increment counter and start loop again
+	RCALL VIKTOR_START_TIMER_LOOP; If 1 second timer loop reached 0, increment counter and start loop again
 	RCALL INC_COUNT
 	RET
 
@@ -201,31 +200,32 @@ VIKTOR_COMPLETE_TIMER:; resolves a 0.02 second timer ending
 	RET
 
 .ORG 700
-VIKTOR_START_TIMER:; starts 0.02 second timer
+VIKTOR_START_TIMER:; Starts 0.02 second timer
 	LDI R25, 0b00000000
 	LDI R25, (1 << TOV0)
-	OUT TIFR0, R25; resets TOV0
+	OUT TIFR0, R25; Resets TOV0
 
-	OUT TCNT0, R2; loads pre-load timer value
+	OUT TCNT0, R2; Loads pre-load timer value
 	LDI R25, 0b00000101
-	OUT TCCR0B, R25; configure timer to start with 1024 scale multiplier
+	OUT TCCR0B, R25; Configure timer to start with 1024 scale multiplier
 
 	RET
 
 .ORG 725
-VIKTOR_START_TIMER_LOOP:; starts 1 second timer loop
+VIKTOR_START_TIMER_LOOP:; Starts 1 second timer loop
 	LDI R25, 50
-	MOV R4, R25; set loop counter to 50
+	MOV R4, R25; Set loop counter to 50
 	LDI R25, 0xFF
-	MOV R3, R25; enable timer feature, causing update timer to be called in each main loop iteration
-	RCALL VIKTOR_START_TIMER; start 1st 0.02 second timer
+	MOV R3, R25; Enables timer feature, causing update timer to be called in each main loop iteration
+	RCALL VIKTOR_START_TIMER; Starts 1st 0.02 second timer
 	RET
 
 .ORG 750
-VIKTOR_ENABLE_TIMER: ; call when timer first enabled with button toggle
+VIKTOR_ENABLE_TIMER:; Call when timer first enabled with button toggle
 	RCALL INC_COUNT
 	RCALL VIKTOR_START_TIMER_LOOP
 	RET
+
 ; For your first project, you will design a simple, self-contained AVR-based device (Simon Board) that will, at a minimum, monitor two keys – one is a positive key that
 ; increments the counter, and the other is a negative key that decrements the counter; display the current count in binary on a set of 5 LEDs; and sound an alarm when
 ; the count “turns over” (cycles from a binary 30 to 0 or 0 to 30). The counter should be 0 initially.
