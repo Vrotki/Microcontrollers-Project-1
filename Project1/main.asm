f0f3cefebc1f28421d16873559bf0ced94279268
Vrotki <51521287+Vrotki@users.noreply.github.com>
2023-10-28 14:09:31 -0500

    Documentation improvements

--- Project1/main.asm
+++ Project1/main.asm
@@ -46,54 +46,61 @@ start:
 		IN R19, PINA; Sets R19 to current buttons pressed
 		MOV R21, R19; Copies current buttons pressed values to R21 for comparison
 
-		; Monitors PINA, 0 - if bit was 0 and is now 1, positive key was just released and the counter should be incremented, returning to 0 if passing 31
+		; Detects SW1 release for increment- if PINA, 0 was 0 and is now 1, the positive key was just released and the counter should be incremented
 		LSR R20; Shifts positive key previous pressed to carry bit
 		BRLO FINISH_POSITIVE_SHIFT; Branches if c == 1 - skips if button was not being pressed
-								  ; While INC_COUNT needs to be skipped, subsequent buttons will only work if R21 is still
 		LSR R21; Shifts positive key current pressed to carry bit
 		BRSH POSITIVE_NOT_RELEASED; Branches if c == 0 - skip if button is still being pressed
-		CALL INC_COUNT; Increments if button was being pressed and is no longer being pressed
+
+		; All button press detections are in this format - any code in this middle section will run if the corresponding button was just released
+		CALL INC_COUNT; Called if PA0 changed from 0 to 1
+
 		POSITIVE_NOT_RELEASED:
 
-		; Monitors PINA, 1 - if bit was 0 and is now 1, negative key was just released and the counter should be decremented, returning to 31 if passing 0
+
+		; Detects SW2 release for decrement
 		LSR R20; Shifts negative key previous pressed to carry bit
 		BRLO FINISH_NEGATIVE_SHIFT; Branches if c == 1 - skip if button was not being pressed
 								  ; While DEC_COUNT needs to be skipped, subsequent buttons will only work if R21 is still shifted
 		LSR R21; Shifts negative key current pressed to carry bit
 		BRSH NEGATIVE_NOT_RELEASED; Branches if c == 0 - skip if button is still being pressed
 		
-		CALL DEC_COUNT; Decrements if button was being pressed and is no longer being pressed
+		CALL DEC_COUNT; Called if PA1 changed from 0 to 1
 		NEGATIVE_NOT_RELEASED:
 
-		; Detect button releases for individual features
+		; Detects SW3 release for Viktor timer enable
 		LSR R20
 		BRLO FINISH_TIMER_SHIFT; 
 		LSR R21
 		BRSH TIMER_NOT_RELEASED
+		; Called if PA2 changed from 0 to 1
 		COM R3; Toggles timer enable
 		SBRC R3, 0; If timer not enabled (if R3 bit 0 is 0), skips
 		RCALL VIKTOR_ENABLE_TIMER; If timer just enabled, starts timer loop
 		TIMER_NOT_RELEASED:
 
+		; Detects SW4 release for Sydney double count
 		LSR R20
 		BRLO FINISH_DOUBLECOUNT_SHIFT
 		LSR R21
 		BRSH DOUBLECOUNT_NOT_RELEASED
-		RCALL SYDNEY_DOUBLECOUNT
+		RCALL SYDNEY_DOUBLECOUNT; Called if PA3 changed from 0 to 1
 		DOUBLECOUNT_NOT_RELEASED:
 
+		; Detects SW6 release for Sydney double count (SW5 skipped, as its pin corresponds to PE6, and this button press detection relies on changes in the contents of PA)
 		LSR R20
 		BRLO FINISH_HALFCOUNT_SHIFT
 		LSR R21
 		BRSH HALFCOUNT_NOT_RELEASED
-		RCALL SYDNEY_HALFCOUNT
+		RCALL SYDNEY_HALFCOUNT; Called if PA4 changed from 0 to 1
 		HALFCOUNT_NOT_RELEASED:
 
+		; Detects SW7 release for Jorge blink shift
 		LSR R20
 		BRLO FINISH_BLINK_SHIFT
 		LSR R21
 		BRSH BLINK_NOT_RELEASED
-		RCALL JORGE_BLINK_TEST
+		RCALL JORGE_BLINK_TEST; Called if PA5 changed from 0 to 1
 		BLINK_NOT_RELEASED:
 
 		; Format to add new feature called featurename on the next available button - put the code here
@@ -116,6 +123,9 @@ start:
 		COM R16
 
 		RJMP MAIN_LOOP
+
+	; Both R20 and R21 need to shift for the previous and current status of the next button's PAx pin to be in bit position 0 - if branching to skip a section
+	;	of code before both are shifted, the other needs to be shifted as well for subsequent buttons to be detected correctly
 	FINISH_POSITIVE_SHIFT:
 		LSR R21
 		RJMP POSITIVE_NOT_RELEASED
@@ -139,7 +149,7 @@ start:
 		RJMP featurename_NOT_RELEASED
 
 .ORG 350
-CHECK_LOW_OVERFLOW:
+CHECK_LOW_OVERFLOW:; Called after each decrement - plays sound and returns counter to 30 if overflowed past 0
 	SBRC R16, 7; If 128's place is 1, negative overflow just occurred
 	RCALL SOUND; If overflowed, play sound
 
@@ -149,7 +159,7 @@ CHECK_LOW_OVERFLOW:
 	RET
 
 .ORG 375
-CHECK_HIGH_OVERFLOW:
+CHECK_HIGH_OVERFLOW:; Called after each increment - plays sound and subtracts 31 until below 31 if overflowed past 30
 	MOV R25, R16
 	LDI R29, 0b11100000
 	AND R25, R29
@@ -167,22 +177,22 @@ CHECK_HIGH_OVERFLOW:
 	RET
 
 .ORG 400
-INC_COUNT:
+INC_COUNT:; Called to increment counter, while also checking for/resolving overflow and delaying to ignore button presses briefly after button released
 	INC R16
 	RCALL CHECK_HIGH_OVERFLOW
 	RCALL BUTTON_DELAY
 	RET
 
 .ORG 425
-DEC_COUNT:
+DEC_COUNT:; Called to decrement counter, while also checking for/resolving overflow and delaying to ignore button presses briefly after button released
 	DEC R16
 	RCALL CHECK_LOW_OVERFLOW
 	RCALL BUTTON_DELAY
 	RET
 
 .ORG 450
-SOUND_DELAY: ; For 1000 HZ frequency, each half-wave should have a 0.5 ms delay, or about 8000 machine cycles
-			 ; 65 and 40 were calculated to result in 8002 machine cycles for each RCALL SOUND_DELAY
+SOUND_DELAY:; Creates the proper delay for a half wave of a ~1000 Hz frequency sound (0.5 ms delay -> 8000 machine cycles -> each RCALL SOUND_DELAY takes 8002 machine cycles)
+	; 65 and 40 were calculated to result in 8002 machine cycles for each RCALL SOUND_DELAY
 	LDI R29, 65
 	repeat_1:
 	LDI R30, 40
@@ -194,7 +204,7 @@ SOUND_DELAY: ; For 1000 HZ frequency, each half-wave should have a 0.5 ms delay,
 	RET
 	
 .ORG 475
-SOUND:
+SOUND:; Called when overflow occurs, repeats 200 waves of a ~1000 Hz frequency sound
 	LDI R25, 200
 	repeat_sound:
 	SBI PORTE, 4
@@ -206,32 +216,35 @@ SOUND:
 	RET
 
 .ORG 500
-BUTTON_DELAY:  LDI R31, 10
-	a:  LDI R30, 0xFF
+BUTTON_DELAY:; Called by button handlers, briefly ignores further button input to avoid issue with button input pin incorrectly changing value while button is still held down
+	LDI R31, 10
+	button_delay_a:  LDI R30, 0xFF
 
-	b:  LDI R29, 0xFF
+	button_delay_b:  LDI R29, 0xFF
 
-	c:  NOP
+	button_delay_c:  NOP
 		NOP
 		DEC R29
-		BRNE c
+		BRNE button_delay_c
 
 		DEC R30
-		BRNE b 
+		BRNE button_delay_b 
 
 		DEC R31
-		BRNE a
+		BRNE button_delay_a
 		RET
 
+; Viktor - My individual feature has a toggle button that uses a loop of asynchronous timers to increment the counter each second without blocking the rest of the program
 .ORG 650
-VIKTOR_UPDATE_TIMER:; Checks each main loop for whether a 0.02 second timer
+VIKTOR_UPDATE_TIMER:; Called in each main loop if timer feature is enabled, checks for whether a 0.02 second timer just finished
 	IN R25, TIFR0
 	SBRC R25, TOV0; Skips command if TOV0 is 0
 	RCALL VIKTOR_COMPLETE_TIMER; If TOV0 is 1, 0.02 second timer just completed
 	RET
 
 .ORG 675
-VIKTOR_COMPLETE_TIMER:; Resolves a 0.02 second timer ending
+VIKTOR_COMPLETE_TIMER:; Called after a single 0.02 second timer finishes - either starts a new 0.02 second timer, or increments count and starts a new loop if the loop of
+					  ;		50 0.02 second timers just finished
 	DEC R4; Decrements timer loop each time timer finishes
 	BRNE CONTINUE_TIMER_LOOP; If 1 second timer loop not done, start timer again
 
@@ -244,7 +257,7 @@ VIKTOR_COMPLETE_TIMER:; Resolves a 0.02 second timer ending
 	RET
 
 .ORG 700
-VIKTOR_START_TIMER:; Starts 0.02 second timer
+VIKTOR_START_TIMER:; Called before starting a 0.02 second timer to configure timer 0
 	LDI R25, 0b00000000
 	LDI R25, (1 << TOV0)
 	OUT TIFR0, R25; Resets TOV0
@@ -256,7 +269,7 @@ VIKTOR_START_TIMER:; Starts 0.02 second timer
 	RET
 
 .ORG 725
-VIKTOR_START_TIMER_LOOP:; Starts 1 second timer loop
+VIKTOR_START_TIMER_LOOP:; Called when timer first enabled or when a previous loop finishes, starts a 1 second loop of 50 0.02 second timers
 	LDI R25, 50
 	MOV R4, R25; Set loop counter to 50
 	LDI R25, 0xFF
@@ -265,7 +278,7 @@ VIKTOR_START_TIMER_LOOP:; Starts 1 second timer loop
 	RET
 
 .ORG 750
-VIKTOR_ENABLE_TIMER:; Call when timer first enabled with button toggle
+VIKTOR_ENABLE_TIMER:; Called when timer first enabled with button toggle, increments immediately to show responsiveness and starts the first 1 second timer loop
 	RCALL INC_COUNT
 	RCALL VIKTOR_START_TIMER_LOOP
 	RET
