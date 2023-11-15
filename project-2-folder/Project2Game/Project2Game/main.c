734243d758fab50a2c13f3e9b010e9f1293e2aab
Vrotki <51521287+Vrotki@users.noreply.github.com>
2023-11-15 14:32:23 -0600

    Added game workspace

---
+++ project-2-folder/Project2Game/Project2Game/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project2Game.c
+ *
+ * Created: 11/15/2023 2:30:41 PM
+ * Author : vikto
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
