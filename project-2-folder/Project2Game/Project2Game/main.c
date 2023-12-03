5129d71ee927111a9c72d4d060c51279c75db446
Vrotki <51521287+Vrotki@users.noreply.github.com>
2023-12-03 13:47:37 -0600

    Refactor, planned out remaining game functionality

--- project-2-folder/Project2Game/Project2Game/main.c
+++ project-2-folder/Project2Game/Project2Game/main.c
@@ -1,27 +1,36 @@
-/*
- * Project2Game.c
- *
- * Created: 11/15/2023 2:30:41 PM
- * Author : vikto
- */ 
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
 #define F_CPU 16000000UL
 
-int sec = 999;
+int sec = 0;
+int game_score = 0;
 int previous_buttons_pressed = 0b11111111; // Set previous buttons to not pressed
 int current_buttons_pressed = 0b11111111; // Set current buttons to not pressed
-int previous_E_buttons_pressed = 1; // Set previous PE5 to not pressed
-int current_E_buttons_pressed = 1; // Set current PE5 to not pressed
+int previous_E_buttons_pressed = 0b11111111; // Set previous PE5 to not pressed
+int current_E_buttons_pressed = 0b11111111; // Set current PE5 to not pressed
 
 bool startup = true;
 bool game = false;
 
-ISR(TIMER1_COMPA_vect){		//Every Second the Interrupt Service Routine will be performed
+ISR(TIMER1_COMPA_vect){	// Every Second the Interrupt Service Routine will be performed
 	TCNT1 = 0x00;
-	sec--;
+	if(game)
+	{
+		sec--;
+		// Pseudo-randomly generate a number from 1-6:
+		//	1 corresponds to SW1 (PA0) and LED1 (PD0)
+		//	2 corresponds to SW2 (PA1) and LED2 (PD1)
+		//  3 corresponds to SW3 (PA2) and LED3 (PD2)
+		//	4 corresponds to SW7 (PA5) and LED7 (PD5)
+		//	5 corresponds to SW8 (PA6) and LED8 (PD6)
+		//	6 corresponds to SW9 (PA7) and LED9 (PD7)
+		// During the game, a new number will be chosen each second - the number must be different from the previous number (keep choosing new numbers until different from previous)
+		// When a number is chosen, the corresponding LED will light up
+		// If the corresponding button is pressed before the next number is chosen, the score will increment, the LED will turn off, and a happy sound will play
+		//		- nothing else will happen until the next number is chosen
+		// If the corresponding button is not pressed before the next number is chosen, the LED will turn off and an unpleasant sound will play
+	}
 }
 
 void timer_init_ctc(){
@@ -49,9 +58,9 @@ void sound(){
 }
 
 void sound_delay(){
-	TCNT0 = 130; //125 * 64 = 8000
-	TCCR0B = 0b00000011; //64 timer pre-scale
-	while(!(TIFR0 << TOV0)){} //waits 8000 machine cycles for 1000 Hz half wave
+	TCNT0 = 130; // 125 * 64 = 8000
+	TCCR0B = 0b00000011; // 64 timer pre-scale
+	while(!(TIFR0 << TOV0)){} // Waits 8000 machine cycles for 1000 Hz half wave
 	TCCR0B = 0;
 	TIFR0 = (1 << TOV0);
 	return;
@@ -65,11 +74,15 @@ void check_buttons(){
 		if(previous_button_pressed && !current_button_pressed){
 			switch(button_index){
 				case 0: // SW1
+				if(startup){
+					start_game();
+				}
 				sound();
 				break;
 				
 				case 1: // SW2
 				sound();
+				game_score++;
 				break;
 				
 				case 2: // SW3
@@ -108,28 +121,28 @@ void check_buttons(){
 	previous_E_buttons_pressed = current_E_buttons_pressed;
 }
 
-void start_up(){
-	while(startup){
-		if(~PINA & (1<<PINA0)){			// If SW1 is pressed, start the game
-			while(~PINA & (1<<PINA0));
-			sec = 30;
-			PORTD = 0xFF; // Set PD (LEDs are off)
-			startup = false;
-			game = true;
-		}
-	}
+void start_game(){ // If SW1 is pressed during start mode, start the game
+	sec = 5;
+	PORTD = 0xFF; // Set PD (LEDs are off)
+	startup = false;
+	game = true;
 }
 
-void game_up(){
-    while (game) {
-	    check_buttons();
+void end_game(){
+	PORTD = game_score ^ 0b11111111; // Set PD to display score
+	game_score = 0;
+	startup = true;
+	game = false;
+	sound();
+}
 
-	    if(sec == 0){	// After 30 Seconds, game is finished
-		    PORTD = 0x00;	// Clear PD (LEDs are on)
-		    sound();
-		    game = false;
-	    }
-    }
+void start_update(){ // Conducts any main-loop updates during start mode
+	check_buttons();
+} 
+
+void game_update(){ // Conducts any main-loop updates during game mode
+	check_buttons();
+	if(sec == 0) end_game(); // After 30 Seconds, game is finished
 }
 
 int main(void)
@@ -146,9 +159,11 @@ int main(void)
 	timer_init_ctc();
 	sei();
 	timer1_start();
-	
-	start_up();
-	
-	game_up();
+
+	while(1){
+		if(startup) start_update();
+		if (game) game_update();
+	}
+	return 0;
 }
 
